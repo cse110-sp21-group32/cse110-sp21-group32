460c586f132a4cd9a3db2b7816a97e8ca93f08d0
HeBilLi <50161529+HeBilLi@users.noreply.github.com>
2021-05-21 06:00:27 +0800

    Update sources/Scripts/script.js
    Co-authored-by: gtierrezandres <a8gutier@ucsd.edu>

--- sources/Scripts/script.js
+++ sources/Scripts/script.js
@@ -1,6 +1,6 @@
 // script.js
 
-import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
+import { router } from "./router.js"; // Router imported so you can use it to manipulate your SPA app here
 const setState = router.setState;
 var form;
 
