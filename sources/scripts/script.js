e73d5522aa1e9e3a7d110bf0d4a453d3487c80d8
HeBilLi <50161529+HeBilLi@users.noreply.github.com>
2021-06-10 13:09:38 +0800

    Remove log print

--- sources/scripts/script.js
+++ sources/scripts/script.js
@@ -65,7 +65,6 @@ document.addEventListener("click", (e) => {
       newBullet.type="note";
     }
     bulletObj.bullet = newBullet;
-    console.log("CHeck");
   }
 
   // Click editCategory button
